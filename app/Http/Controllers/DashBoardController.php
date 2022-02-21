a79bb710f5dd6b41cd337adb02507d71bbfb3ae1
Chipango <35219186+KahiluChipango@users.noreply.github.com>
2022-02-21 21:25:40 +0200

    multi auth configs from laratrust

---
+++ app/Http/Controllers/DashBoardController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashBoardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(Auth::user()->hasRole('superadmin')){
+            return redirect()->route('super-admin.index');
+
+        }elseif(Auth::user()->hasRole('admin')){
+            return redirect()->route('admin.index');
+
+        }elseif(Auth::user()->hasRole('frontdesk')){
+            return redirect()->route('front-desk.index');
+
+        }elseif(Auth::user()->hasRole('accounts')){
+            return redirect()->route('accounts.index');
+
+        }elseif(Auth::user()->hasRole('agency')){
+            return redirect()->route('agency.index');
+
+        }elseif(Auth::user()->hasRole('valuation')){
+            return redirect()->route('valuation.index');
+
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
