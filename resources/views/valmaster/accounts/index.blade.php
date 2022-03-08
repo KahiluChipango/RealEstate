e9040505342261ab3483eac4b8e2610e1cafa5cc
Peter-SnS <peter@sparknspur.com>
2022-03-08 09:55:51 +0200

    accounts.index.blade.php edited

--- resources/views/valmaster/accounts/index.blade.php
+++ resources/views/valmaster/accounts/index.blade.php
@@ -73,7 +73,7 @@
                                     @elseif($client->fee_status == 0)
                                         {{--invoice Button--}}
                                         <a class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                           href="{{ route('valmaster.accounts.send.inv') }}"
+                                           href="{{ route('valmaster.accounts.send.invoice') }}"
                                            role="button">
                                             Invoice
                                         </a>
