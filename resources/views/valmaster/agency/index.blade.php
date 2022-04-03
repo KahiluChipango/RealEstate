52e34ebd024a064165aa141ad18741125e2f55d2
Chipango <35219186+KahiluChipango@users.noreply.github.com>
2022-04-03 22:56:05 +0200

    Sms And Some some css perfecting

--- resources/views/valmaster/agency/index.blade.php
+++ resources/views/valmaster/agency/index.blade.php
@@ -59,7 +59,7 @@
 
         </div>
 
-        <div class="max-w-7xl overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class=" divide-y divide-dashed shadow overflow-hidden border border-indigo-400 sm:rounded-lg ">
 
--- resources/views/valmaster/front-desk/table-content.blade.php
+++ resources/views/valmaster/front-desk/table-content.blade.php
@@ -50,9 +50,10 @@
             @endif
 
         </div>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-x-auto shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class=" divide-y divide-dashed shadow overflow-hidden border border-indigo-400 sm:rounded-lg ">
+
 
                 <x-table.table>
                             <x-slot name="header">
