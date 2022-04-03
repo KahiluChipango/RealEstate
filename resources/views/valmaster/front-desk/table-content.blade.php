4c7d73fac69fc7cdb108c3609a604b6524df982b
Chipango <35219186+KahiluChipango@users.noreply.github.com>
2022-04-04 00:40:16 +0200

    Sms And Some some css perfecting

--- resources/views/valmaster/front-desk/table-content.blade.php
+++ resources/views/valmaster/front-desk/table-content.blade.php
@@ -50,7 +50,7 @@
             @endif
 
         </div>
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="max-w-full overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class=" divide-y divide-dashed shadow overflow-hidden border border-indigo-400 sm:rounded-lg ">
 
