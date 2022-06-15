88791d0fe0336dc949c7cec74411110f8e83a03e
Chipango <35219186+KahiluChipango@users.noreply.github.com>
2022-06-15 18:43:19 +0200

    Change of Logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 
-<img src="{{asset('logo/SG Logo.png')}}" alt="Image" loading="lazy" class="shadow-lg h-16 w-auto sm:h-10">
+<img src="{{asset('logo/RealEstateLogo.png')}}" alt="Image" loading="lazy" class="shadow-lg h-16 w-auto sm:h-10">
 
